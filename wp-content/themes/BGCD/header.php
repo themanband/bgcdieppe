519467307c30961f4bc362bac6f63faa709778f9
dylan <dylan@Dylans.WebProgramming>
2019-01-07 14:22:59 -0400

    Creating New Theme

---
+++ wp-content/themes/BGCD/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title><?php bloginfo('title')?></title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>"/>
+     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">--> 
+   
+</head>
+<header>
+<h3 style="float:right;padding:.5em;"><a href="">Contact us</a> | <a href="">Locate us</a></h3>
+</header>
+<nav>
+    <?php wp_nav_menu()?>
+</nav>
+
+<div id ="container">
